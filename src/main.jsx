37d32287e6a9d5aa02a48994554206cdf16e3f37
Zane Wang <zane@Zane-MacBook-Pro.local>
2025-04-30 14:06:11 +1000

    Exercise 2.1 - 2.5

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
