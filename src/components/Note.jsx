5041cf96fa646b744e59e71a69fd59f3dd41a275
Zane Wang <zane@Zane-MacBook-Pro.local>
2025-08-28 21:20:36 +1000

    bug fixed for id issue

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,7 +4,7 @@ const Note = ({ note, toggleImportance }) => {
   return (
     <li>
       {note.content}
-      <button onClick={toggleImportance}>{label}</button>
+      <button onClick={toggleImportance}> {label} </button>
     </li>
   )
 }
